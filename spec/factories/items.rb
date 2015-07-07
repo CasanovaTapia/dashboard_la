674a357ed58d9684c81eeaa5839ff0cb44b5d21e
Will Carron <wacarron27@gmail.com>
2015-07-07 15:36:49 -0700

    added sessions and users controllers

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :item do
     name "MyString"
-price "9.99"
-cost "9.99"
-description "MyString"
+    price "9.99"
+    cost "9.99"
+    description "MyString"
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-email "MyString"
-phone "MyString"
-address "MyString"
+      name "MyString"
+      email "MyString"
+      phone "MyString"
+      address "MyString"
   end
 
 end
